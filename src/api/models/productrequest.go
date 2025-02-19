131f77bfec37108540f6b4d3ebc7fad6b4fea34f
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2025-02-19 15:13:47 +0600

    Revert "Add product API"

--- src/api/models/productrequest.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type ProductRequest struct {
-	Name            string `json:"product_name"`
-	Description     string `json:"product_description"`
-	TransactionHash string `json:"transaction_hash"`
-}
