eeefd597daebd6d8d7b714190895f228285d87b7
Fagun <mdabuzaforfagun@gmail.com>
2025-02-19 14:45:49 +0600

    add table script

---
+++ src/db/script.sql
@@ -0,0 +1,13 @@
+CREATE TABLE products (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name TEXT NOT NULL,
+	seller TEXT NOT NULL,
+    description TEXT,
+    price TEXT NOT NULL,
+    stock TEXT NOT NULL,
+    transaction_hash TEXT NOT NULL,
+    on_chain_id TEXT NOT NULL,
+    status INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    creation_completed_at TIMESTAMPTZ
+);
